3dc044fb2f7ba70c4d7ec0bd289fba08f28da61c
Dboy <894230813@qq.com>
2021-11-08 17:57:14 +0800

    开源协议

--- slotting-api/src/main/java/com/dboy/slotting/api/Slotting.kt
+++ slotting-api/src/main/java/com/dboy/slotting/api/Slotting.kt
@@ -1,6 +1,21 @@
 package com.dboy.slotting.api
 
 /**
+ *
+ *  Copyright 2021 Dboy233
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
  * - 文件描述: 消息接受接口。在自己的项目中实现此接口。
  * - app/build.gradle：
  * ```groovy
--- slotting-gradle-plugin/src/main/java/com/dboy/slotting/SlottingPlugin.kt
+++ slotting-gradle-plugin/src/main/java/com/dboy/slotting/SlottingPlugin.kt
@@ -9,9 +9,23 @@ import com.dboy.slotting.transform.SlottingTransform
 import com.dboy.slotting.utils.SlottingLog
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-
 /**
- * - 文件描述:
+ *
+ *  Copyright 2021 Dboy233
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ * - 文件描述: Slotting 插件
  * @author DBoy
  * @since 2021/11/4 08:31
  */
@@ -19,7 +33,6 @@ class SlottingPlugin : Plugin<Project> {
     override fun apply(target: Project) {
         if (target.plugins.hasPlugin("com.android.application")) {
             SlottingLog.info("开始注册插件")
-
 
             //注册
             val appExtension = target.extensions.findByType(AppExtension::class.java)
